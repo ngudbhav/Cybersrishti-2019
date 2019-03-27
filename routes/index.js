a3b396f9ebf1c7db86b19168355b3a943ad95350
unknown <ngudbhav05@hotmail.com>
2019-03-27 23:16:56 +0530

    Contact us form added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+	host: "smtp-mail.outlook.com", // hostname
+	secureConnection: false, // TLS requires secureConnection to be false
+	port: 587, // port for secure SMTP
+	tls: {
+		ciphers:'SSLv3'
+	},
+	auth: {
+		user: 'ngudbhavtest@outlook.com',
+		pass: 'NGUdbhav'
+	}
+});
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.post('/contact/', function(req, res, next) {
+	var mailOptions = {
+		from: 'ngudbhavtest@outlook.com', // sender address (who sends)
+		to: 'ngudbhav05@hotmail.com', // list of receivers (who receives)
+		subject: 'Feedback my wesbite', // Subject line
+		text: 'Hello '+req.body.kname+ ' '+ req.body.kmsg + ' by '+req.body.kemail // plaintext body
+	};
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+		}
+		else{
+		}
+		res.redirect('/');
+	});
+});
 module.exports = router;
