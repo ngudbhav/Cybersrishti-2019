2d446ba75b37e9d5cd939f33fc542481b744a7c0
Karishma Agarwal <karishag1998@gmsil.com>
2019-04-08 18:49:23 +0530

    3 events updated

--- WEB/routes/index.js
+++ WEB/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
-var db = require('../db.js');
+//var db = require('../db.js');
 var transporter = nodemailer.createTransport({
 	host: "smtp-mail.outlook.com", // hostname
 	secureConnection: false, // TLS requires secureConnection to be false
@@ -35,12 +35,12 @@ router.post('/contact/', function(req, res, next) {
 });
 router.post('/event/app/', function(req, res, next){
 	console.log(req.body.date);
-	db.query('insert into question values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [parseInt(req.body.kroll), req.body.kname, req.body.kemail, req.body.knumber, req.body.kq1, req.body.kq2, req.body.date, req.body.kq3, req.body.kq4, req.body.kq5], function(error, results){
+	/*db.query('insert into question values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [parseInt(req.body.kroll), req.body.kname, req.body.kemail, req.body.knumber, req.body.kq1, req.body.kq2, req.body.date, req.body.kq3, req.body.kq4, req.body.kq5], function(error, results){
 		if(error) throw error;
 		else{
 			res.send('1');
 		}
-	});
+	});*/
 });
 router.get('/download/android/', function(req, res, next){
 	res.download(__dirname + '/app.apk');
